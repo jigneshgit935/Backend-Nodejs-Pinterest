7a812177a290add84e3b53d638d73838d704925b
Jignesh <jignesh.sharma.707@gmail.com>
2023-11-22 23:12:40 +0530

    flash message commited while login error

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/feed', function (req, res, next) {
 });
 
 router.get('/login', function (req, res, next) {
-  res.render('login');
+  res.render('login', { error: req.flash('error') });
 });
 
 router.get('/profile', isLoggedIn, function (req, res, next) {
@@ -43,6 +43,7 @@ router.post(
   passport.authenticate('local', {
     successRedirect: '/profile',
     failureRedirect: '/login',
+    failureFlash: true,
   }),
   function (req, res, next) {}
 );
